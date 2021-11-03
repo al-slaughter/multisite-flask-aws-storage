3f7585985003fcf23bdc4104ac69901f2e7dfceb
Al Slaughter <alyssa.slaughter@rackspace.com>
2021-11-02 21:49:39 -0500

    jwt test

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ import base64
 import json
 
 # Step 1: Get the key id from JWT headers (the kid field)
-header = request.environ.get('x-amzn-oidc-data')
-print(header)
+encoded_jwt = request.environ.get('x-amzn-oidc-data')
 # encoded_jwt = headers.dict['x-amzn-oidc-data']
-# jwt_headers = encoded_jwt.split('.')[0]
-# decoded_jwt_headers = base64.b64decode(jwt_headers)
-# decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
-# decoded_json = json.loads(decoded_jwt_headers)
-# kid = decoded_json['kid']
-#
+jwt_headers = encoded_jwt.split('.')[0]
+decoded_jwt_headers = base64.b64decode(jwt_headers)
+decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
+decoded_json = json.loads(decoded_jwt_headers)
+kid = decoded_json['kid']
+print(kid)
+
 # # Step 2: Get the public key from regional endpoint
 # url = 'https://public-keys.auth.elb.' + region + '.amazonaws.com/' + kid
 # req = requests.get(url)
