8c61716c715d89bbc2752ec662db5661ef7a0382
Al Slaughter <alyssa.slaughter@rackspace.com>
2021-11-02 21:57:52 -0500

    Get session data

--- app.py
+++ app.py
@@ -7,23 +7,6 @@ import requests
 import base64
 import json
 
-# Step 1: Get the key id from JWT headers (the kid field)
-encoded_jwt = request.environ.get('x-amzn-oidc-data')
-# encoded_jwt = headers.dict['x-amzn-oidc-data']
-jwt_headers = encoded_jwt.split('.')[0]
-decoded_jwt_headers = base64.b64decode(jwt_headers)
-decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
-decoded_json = json.loads(decoded_jwt_headers)
-kid = decoded_json['kid']
-print(kid)
-
-# # Step 2: Get the public key from regional endpoint
-# url = 'https://public-keys.auth.elb.' + region + '.amazonaws.com/' + kid
-# req = requests.get(url)
-# pub_key = req.text
-#
-# # Step 3: Get the payload
-# payload = jwt.decode(encoded_jwt, pub_key, algorithms=['ES256'])
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
@@ -34,6 +17,23 @@ BUCKET = "dev-finalproject-img-mgr20211101145336209100000001"
 def home():
     contents = list_files(BUCKET)
     print(contents)
+    # Step 1: Get the key id from JWT headers (the kid field)
+    encoded_jwt = request.environ.get('x-amzn-oidc-data')
+    # encoded_jwt = headers.dict['x-amzn-oidc-data']
+    jwt_headers = encoded_jwt.split('.')[0]
+    decoded_jwt_headers = base64.b64decode(jwt_headers)
+    decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
+    decoded_json = json.loads(decoded_jwt_headers)
+    kid = decoded_json['kid']
+    print(kid)
+
+    # # Step 2: Get the public key from regional endpoint
+    # url = 'https://public-keys.auth.elb.' + region + '.amazonaws.com/' + kid
+    # req = requests.get(url)
+    # pub_key = req.text
+    #
+    # # Step 3: Get the payload
+    # payload = jwt.decode(encoded_jwt, pub_key, algorithms=['ES256'])
     return render_template('index.html')
 
 
